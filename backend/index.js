c496b8b933d747a2c74ebaf9eb7230ff61d339c9
PASULGE <paul.sge.eintracht@gmail.com>
2024-10-20 18:44:30 +0200

    DB-Verbindung aufgebaut

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const cors = require("cors");
 const mainPage = require("./route/mainPage.route.js")
+const pool = require("./model/db.js")
 
 require("dotenv").config();
 const app = express();
 
 app.use(cors());
 
-const schema = process.env.DBSCHEMA;
-const PORT = process.env.PORT;
-
 app.use("/",mainPage);
 
-app.listen(PORT, () => {
-    console.log(`Server läuft http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server läuft http://localhost:${process.env.PORT}`);
 });
+
+pool.connect()
+    .then(()=> console.log("DB erreicht"))
+    .catch(err => console.error("Fehler DB", err.stack))
