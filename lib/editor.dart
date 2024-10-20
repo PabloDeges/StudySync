fb261ef4bf790f8fe62ce0bbfd943ad8ffda1b26
pablodeges <degespablo@gmail.com>
2024-10-20 21:07:11 +0200

    Dropdowns added

--- lib/editor.dart
+++ lib/editor.dart
@@ -1,3 +1,5 @@
+import 'dart:nativewrappers/_internal/vm/lib/internal_patch.dart';
+
 import 'package:flutter/material.dart';
 
 class EditorView extends StatelessWidget {
@@ -9,15 +11,36 @@ class EditorView extends StatelessWidget {
     DropdownMenuEntry(value: "BWL", label: "Geldhaben")
   ];
 
+  static const semesterAuswahl = [
+    DropdownMenuEntry(value: "IB1", label: "Informatik 1. Semester"),
+    DropdownMenuEntry(value: "IB3", label: "Informatik 3. Semester"),
+    DropdownMenuEntry(value: "IB5", label: "Informatik 5. Semester")
+  ];
+
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: DropdownMenu<String>(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      DropdownMenu<String>(
+          width: 320,
+          menuStyle: MenuStyle(
+              // surfaceTintColor: WidgetStatePropertyAll(Colors.green),
+              // shadowColor: WidgetStatePropertyAll(Colors.red),
+              // haben irgendwie keinen effekt....
+              backgroundColor: WidgetStatePropertyAll(Colors.blue)),
           label: const Text("Fachbereich"),
           helperText: 'Fachbereich auswählen',
           enableFilter: true,
           // onSelected: (String value) => , add functionality later
           dropdownMenuEntries: fachbereichAuswahl),
-    );
+      DropdownMenu<String>(
+          width: 320,
+          label: const Text("Semester"),
+          helperText: 'Semester auswählen',
+          enableFilter: true,
+          // onSelected: (String value) => , add functionality later
+          dropdownMenuEntries: semesterAuswahl),
+      // ADD RADIO BUTTONS
+    ]));
   }
 }
