637fc346920051d1f977bd7b4598723519df7e3d
pablodeges <degespablo@gmail.com>
2024-10-22 10:44:01 +0200

    editor nun stateful, elemente hinzugefügt, noch kein styling oder dynamik

--- lib/editor.dart
+++ lib/editor.dart
@@ -1,9 +1,16 @@
-import 'dart:nativewrappers/_internal/vm/lib/internal_patch.dart';
+// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
-class EditorView extends StatelessWidget {
+class EditorView extends StatefulWidget {
   const EditorView({super.key});
+
+  @override
+  _EditorViewState createState() => _EditorViewState();
+}
+
+class _EditorViewState extends State<EditorView> {
+  void _kursAuswahlSpeichern() {}
 
   static const fachbereichAuswahl = [
     DropdownMenuEntry(value: "EI", label: "Elektrotechnik / Informatik"),
@@ -17,17 +24,21 @@ class EditorView extends StatelessWidget {
     DropdownMenuEntry(value: "IB5", label: "Informatik 5. Semester")
   ];
 
+  static List<Map> kurse = [
+    {"name": "Mathe 1", "isChecked": false},
+    {"name": "Java 1", "isChecked": false},
+    {"name": "Elektotechnik", "isChecked": false},
+    {"name": "Schlüsselkompetenzen", "isChecked": false},
+    {"name": "Englisch 1", "isChecked": false},
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      // ignore: prefer_const_constructors
       DropdownMenu<String>(
           width: 320,
-          menuStyle: MenuStyle(
-              // surfaceTintColor: WidgetStatePropertyAll(Colors.green),
-              // shadowColor: WidgetStatePropertyAll(Colors.red),
-              // haben irgendwie keinen effekt....
-              backgroundColor: WidgetStatePropertyAll(Colors.blue)),
           label: const Text("Fachbereich"),
           helperText: 'Fachbereich auswählen',
           enableFilter: true,
@@ -40,7 +51,25 @@ class EditorView extends StatelessWidget {
           enableFilter: true,
           // onSelected: (String value) => , add functionality later
           dropdownMenuEntries: semesterAuswahl),
-      // ADD RADIO BUTTONS
+      Column(
+          children: kurse.map((kurs) {
+        return CheckboxListTile(
+            title: Text(kurs["name"]),
+            value: kurs["isChecked"],
+            onChanged: (val) {
+              setState(() {
+                kurs["isChecked"] = val;
+              });
+            });
+      }).toList()),
+      TextButton(
+        style: ButtonStyle(
+            backgroundColor: MaterialStatePropertyAll(Colors.lightBlue),
+            foregroundColor:
+                MaterialStatePropertyAll<Color>(Color(0xffffffff))),
+        onPressed: _kursAuswahlSpeichern,
+        child: Text("Auswahl Speichern"),
+      )
     ]));
   }
 }
