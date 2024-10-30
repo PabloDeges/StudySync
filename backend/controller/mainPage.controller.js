330a5a8474d0514805b1d2c510151e6f17cda9bc
Johan <johan.temme@stud.hs-bochum.de>
2024-10-30 10:58:34 +0100

    routen fuer stundenplan hinzugefuegt

--- backend/controller/mainPage.controller.js
+++ backend/controller/mainPage.controller.js
@@ -1,16 +1,51 @@
-const fs = require("fs");;
+const fs = require("fs");
+const pool = require("../model/db");
 
 const mainPageDisplayInfos = async (req, res) => {
 	try {
 		let daten = readJsonFile("./mainData.json");
 		res.status(200).json(daten);
 	} catch (err) {
-		res.status(500).json({ message: error.message });
+		res.status(500).json({ message: err.message });
 	}
 };
 
+const getStundenplan = async (req, res) => {
+	try {
+		let userid = req.body.userid;
+		let daten = (await pool.query(`SELECT t.id AS terminid, k.kursname, k.kurskuerzel, d.dozname, d.dozkuerzel, t.terminart, t.wochentag, t.startzeit, t.dauer, t.raum, bt.terminstartverzoegerung, bt.kommentar FROM termin AS t LEFT JOIN benutzer_termin AS bt ON t.id = bt.terminid LEFT JOIN doz AS d ON t.dozid = d.id LEFT JOIN kurs AS k ON t.kursid = k.id WHERE bt.benutzerid = ${userid};`)).rows;
+		let startzeit, terminstartverzoegerung;
+		//terminstartverzoegerung wird auf die startzeit gerechnet und anschliessend aus dem datensatz entfernt
+		for (let i = 0; i < daten.length; i++) {
+			startzeit = daten[i].startzeit.split(":");
+			terminstartverzoegerung = daten[i].terminstartverzoegerung;
+			startzeit[0] = parseInt(startzeit[0]) + Math.floor(terminstartverzoegerung/60);
+			startzeit[1] = parseInt(startzeit[1]) + (terminstartverzoegerung % 60);
+			startzeit = startzeit[0]*100 + startzeit[1];
+			daten[i].startzeit = startzeit;
+			delete daten[i].terminstartverzoegerung;
+		}
+		res.status(200).json(daten);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+}
+
+const delTermin = async (req, res) => {
+	try {
+		let userid = req.body.userid;
+		let terminid = req.body.terminid;
+		let daten = await pool.query(`DELETE FROM benutzer_termin WHERE benutzerid = ${userid} AND terminid = ${terminid};`);
+		res.status(200).json({"geloeschteZeilen":daten.rowCount});
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+}
+
 module.exports = {
 	mainPageDisplayInfos,
+	getStundenplan,
+	delTermin
 };
 
 function readJsonFile(filePath) {
--- backend/route/mainPage.route.js
+++ backend/route/mainPage.route.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { mainPageDisplayInfos } = require("../controller/mainPage.controller.js");
+const { mainPageDisplayInfos, getStundenplan, delTermin } = require("../controller/mainPage.controller.js");
+const { changeToSchema } = require("../controller/db.controller.js");
 
 
 router.get("",mainPageDisplayInfos);
+
+router.get("/stundenplan", changeToSchema, getStundenplan);
+
+router.delete("/terminEntfernen", changeToSchema, delTermin);
 
 module.exports = router;
