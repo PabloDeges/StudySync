7d7994c1ad598c1b45cf1913fe2f46450b1c5a53
JohanTemme <johan.temme@stud.hs-bochum.de>
2024-12-31 15:21:17 +0100

    verallgemeinerung und einzel loeschen umgesetzt

--- backend/controller/editor.controller.js
+++ backend/controller/editor.controller.js
@@ -7,8 +7,12 @@ const postCustomStrings = async (req, res) => {
         let kommentar = req.body.comment;
         let link = req.body.link;
         let mail = req.body.mail;
-        let anzMod = (await pool.query(`UPDATE benutzer_termin SET kommentar = '${kommentar}', kurslink = '${link}', kontaktmail = '${mail}' WHERE benutzerid = ${benutzerid} AND terminid = ${terminid}`)).rowCount;
-		res.status(200).json(anzMod);
+        let anzMod = (await pool.query(`UPDATE benutzer_termin SET kommentar = '${kommentar}' WHERE benutzerid = ${benutzerid} AND terminid = ${terminid}`)).rowCount;
+		let terminids = (await pool.query(`SELECT bt.terminid FROM benutzer_termin as bt JOIN termin as t ON bt.terminid = t.id WHERE bt.benutzerid = ${benutzerid} AND t.kursid = (SELECT t.kursid FROM termin as t WHERE t.id = ${terminid});`)).rows;
+        for(let i = 0; i < terminids.length; i++) {
+            anzMod += (await pool.query(`UPDATE benutzer_termin SET kurslink = '${link}', kontaktmail = '${mail}' WHERE benutzerid = ${benutzerid} AND terminid = ${terminids[i].terminid};`)).rowCount;
+        }
+        res.status(200).json(anzMod);
 	} catch (err) {
 		res.status(500).json({ message: err.message });
 	}
@@ -28,7 +32,19 @@ const delKollisionen = async (req, res) => {
     }
 }
 
+const delEinzelTermin = async (req, res) => {
+    try {
+        let benutzerid = req.query.userid;
+        let terminid = req.body.terminid;
+        let anzDel = (await pool.query(`DELETE FROM benutzer_termin WHERE benutzerid = ${benutzerid} AND terminid = ${terminid};`)).rowCount;
+        res.status(200).json(anzDel);
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+}
+
 module.exports = {
     postCustomStrings,
-    delKollisionen
+    delKollisionen,
+    delEinzelTermin
 };
